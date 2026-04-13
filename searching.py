a4f6de43e9fa224748624d4bcea5f3dbc95be742
David <279353@vutbr.cz>
2026-04-13 13:30:19 +0200

    Update

--- searching.py
+++ searching.py
@@ -18,8 +18,6 @@ def read_data(file_name, field):
             - None: If the field is not supported.
     """
     povolene_fieldy = {'unordered_numbers', 'ordered_numbers', 'dna_sequence'}
-    for field in povolene_fieldy:
-        return None
 
     # get current working directory path
     cwd_path = Path.cwd()
@@ -28,13 +26,29 @@ def read_data(file_name, field):
 
     with open(file_path, "r", encoding="UTF-8") as file:
         data = json.load(file)
+        if field not in povolene_fieldy:
+            return None
+        return data.get(field)
 
-    return data.get(field)
+def linear_search(sequence, searched_number):
+
+    positions = []
+
+    for i, number in enumerate(sequence):
+        if number == searched_number:
+            positions.append(i)
+    count = len(positions)
+
+    return {"positions": positions, "count": count}
 
 def main():
 
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
 
+    hledane = 8
+    print(linear_search(sequential_data, hledane))
+
 if __name__ == "__main__":
+
     main()
